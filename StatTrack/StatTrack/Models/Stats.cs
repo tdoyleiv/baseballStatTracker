d7b77bceaa97036946c2b55aa3320a3b70f7b953
Tom Doyle <tfdoyleiv@gmail.com>
2018-03-28 19:07:24 -0500

    Added properties/realtionships to stat model

--- StatTrack/StatTrack/Models/Stats.cs
+++ StatTrack/StatTrack/Models/Stats.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace StatTrack.Models
 {
-    public abstract class Stats
+    public class BattingStats
     {
-
+        public int StatsID { get; set; }
+        public virtual Team Team { get; set; }
+        public int TeamID { get; set; }
+        public virtual Player Player { get; set; }
+        public int PlayerID { get; set; }
+        public int GamesPlayed { get; set; }
+        public int PlateAppearance { get; set; }
+        public int H { get; set; }
+        public int W { get; set; }
+        public int HR { get; set; }
+        public int SO { get; set; }
+        public int LineO { get; set; }
+        public int FlyO { get; set; }
+        public int GIDP { get; set; }
     }
 }
